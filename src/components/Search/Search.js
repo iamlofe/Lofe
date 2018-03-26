854f1be3764232ba876f9516451c1504871b1553
Andrei Taranau <Andrei_Taranau@epam.com>
2018-03-26 17:32:20 +0300

    1000 bug fixed

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,126 +1,127 @@
 import React from 'react';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import { Grid } from 'react-bootstrap';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import {Grid} from 'react-bootstrap';
 import faker from 'faker';
 import SearchResults from './SearchResults';
 import SearchBar from './SearchBar';
 import axios from 'axios';
 
 const makeRequest = () => {
-	const { request, price, rating } = store.getState().filter;
-	axios
-		.get(
-			`http://localhost:3030/search?q=${request}&minprice=${
-			price.min
-			}&maxprice=${price.max}&minrating=${rating.min}&maxrating=${rating.max}`
-		)
-		.then(data => {
-			if (data.status === 200)
-				store.dispatch({ type: 'change_list', results: data.data });
-			else
-				store.dispatch({
-					type: 'change_list',
-					error: []
-				})
-		});
+  const {request, price, rating} = store.getState().filter;
+  console.log(store.getState().filter);
+  axios
+    .get(
+      `http://localhost:3030/search?q=${request}&minprice=${
+        price.min
+      }&maxprice=${price.max}&minrating=${rating.min}&maxrating=${rating.max}`
+    )
+    .then(data => {
+      if (data.status === 200)
+        store.dispatch({type: 'change_list', results: data.data});
+      else
+        store.dispatch({
+          type: 'change_list',
+          results: []
+        });
+    })
+    .catch(error => console.log(error));
 };
 
 const error = (state = '', action) => {
-	switch (action.type) {
-		case 'error_occured':
-			return action.error;
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case 'error_occured':
+      return action.error;
+    default:
+      return state;
+  }
 };
 
 const filterReducer = (
-	state = {
-		request: '',
-		price: {
-			min: 0,
-			max: 1000,
-		},
-		rating: {
-			min: 0,
-			max: 5
-		}
-	},
-	action
+  state = {
+    request: '',
+    price: {
+      min: 0,
+      max: 2000
+    },
+    rating: {
+      min: 0,
+      max: 5
+    }
+  },
+  action
 ) => {
-	switch (action.type) {
-		case 'change_price':
-			setTimeout(() => makeRequest(), 0);
-			return {
-				...state,
-				price: {
-					min: action.min,
-					max: action.max
-				}
-			};
-		case 'change_rating':
-			setTimeout(() => makeRequest(), 0);
-			return {
-				...state,
-				rating: {
-					min: action.min,
-					max: action.max
-				}
-			};
+  switch (action.type) {
+    case 'change_price':
+      setTimeout(() => makeRequest(), 0);
+      return {
+        ...state,
+        price: {
+          min: action.min,
+          max: action.max
+        }
+      };
+    case 'change_rating':
+      setTimeout(() => makeRequest(), 0);
+      return {
+        ...state,
+        rating: {
+          min: action.min,
+          max: action.max
+        }
+      };
 
-		case 'change_request':
-			setTimeout(() => makeRequest(), 0);
+    case 'change_request':
+      setTimeout(() => makeRequest(), 0);
 
-			return { ...state, request: action.request };
+      return {...state, request: action.request};
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
 
 const resultsReducer = (state = [], action) => {
-	switch (action.type) {
-		case 'toggle_liked_flag':
-			//makeRequestToDB();
-			return state.map(
-				result =>
-					result.id === action.id
-						? { ...result, isLiked: !result.isLiked }
-						: result
-			);
-		case 'update_list':
-			return [...state, ...action.results];
-		case 'change_list':
-			return [...action.results];
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case 'toggle_liked_flag':
+      //makeRequestToDB();
+      return state.map(
+        result =>
+          result.id === action.id
+            ? {...result, isLiked: !result.isLiked}
+            : result
+      );
+    case 'update_list':
+      return [...state, ...action.results];
+    case 'change_list':
+      return [...action.results];
+    default:
+      return state;
+  }
 };
 
 const superReducer = combineReducers({
-	results: resultsReducer,
-	filter: filterReducer,
-	error
+  results: resultsReducer,
+  filter: filterReducer,
+  error
 });
 const store = createStore(superReducer);
 class Search extends React.Component {
-	componentDidMount() {
-		//there will be fetch
-		makeRequest();
-	}
-	render() {
-		return (
-			<Provider store={store}>
-				<Grid>
-					<SearchBar />
+  componentDidMount() {
+    //there will be fetch
+    makeRequest();
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <Grid>
+          <SearchBar />
 
-					<SearchResults />
-
-				</Grid>
-			</Provider>
-		);
-	}
+          <SearchResults />
+        </Grid>
+      </Provider>
+    );
+  }
 }
 
 export default Search;
