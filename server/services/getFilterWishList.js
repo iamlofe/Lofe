f0206afefe22a32cbe74eff16e817ca710d23075
Andrei Zenkou <Andrei_Zenkou@epam.com>
2018-03-30 21:16:49 +0300

    filter wish list

--- server/services/getFilterWishList.js
+++ server/services/getFilterWishList.js
@@ -9,7 +9,7 @@ export default async (req, res) => {
 	let result = [];
 	try {
 		wishList = await User.findOne({ _id }, { email: 0, password: 0, username: 0, __v: 0, _id: 0 });
-
+ 
 		for(let i = 0; i < ids.length; i++) {
 			if(wishList.wishList.includes(ids[i])) {
 				result.push(ids[i])
