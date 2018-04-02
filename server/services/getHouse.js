d3cc3ce6c396265cc55bf5b71e6bbbd1a11ddffc
Andrei Zenkou <Andrei_Zenkou@epam.com>
2018-04-02 17:33:26 +0300

    fix about

--- server/services/getHouse.js
+++ server/services/getHouse.js
@@ -4,12 +4,11 @@ import Comment from '../models/comments'
 export default async (req, res, next) => {
 	let { _id } = req.query;
 	console.log(_id)
-	let house, comment;
+	let house, comments;
 	try {
 		house = await House.findOne({ _id }, { __v: 0 });
-		comment = await Comment.find({ _id: house.reviews })
-
-		res.send({ house, comment })
+		comments = await Comment.find({ _id: house.reviews })
+		res.send({ ...house._doc ,reviews: comments});
 	} catch ({ message }) {
 		return next({
 			message: "Can't get all pages",
