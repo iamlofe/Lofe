3da3700d7123750f5e1cf2bbdcd0b6b3c1765800
Iamlooofeee <iamlooofeee@gmail.com>
2018-04-11 18:07:54 +0300

    f

--- server/services/getUser.js
+++ server/services/getUser.js
@@ -3,21 +3,19 @@ import User from "../models/user";
 export default async (req, res, next) => {
 	const { _id } = req.query;
 	let user
-	
+	console.log(_id)
+
 	try {
 		user = await User.findOne({ _id }, { password: 0 }, { __v: 0 });
-		if(!user) {
+		if (!user) {
 			return next({
-				message:"Can't find user",
-				status:400
-			}) 
+				message: "Can't find user",
+				status: 400
+			})
 		} else {
-			return next({
-				message:"success",
-				status:200
-			}) 
+			res.send(user);
 		}
-	}catch({message}) {
+	} catch ({ message }) {
 		return next({
 			status: 400,
 			message: "Not found",
