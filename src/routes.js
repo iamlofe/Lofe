f9859b0adb095b4749f3de706eb1d1f8117bc9bf
antarid <tarand007@gmail.com>
2018-04-16 11:43:26 +0300

    added routes file

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+const baseUrl = 'http://localhost:3030';
+const userPrefix = baseUrl + '/user';
+const housePrefix = baseUrl + '/house';
+
+const urls = {
+  house: {
+    get: {
+      houseBasicInfo: houseId => `${housePrefix}/${houseId}`,
+      houseReviews: houseId => `${housePrefix}/${houseId}/reviews`
+    },
+    post: {
+      search: ({q, rating, price}) =>
+        `${housePrefix}/search?q=${q}&minprice=${price.min}&maxprice=${
+          price.max
+        }&minrating=${rating.min}&maxrating=${rating.max}`,
+      addReview: houseId => `${housePrefix}/addReview`,
+      order: houseID => `${housePrefix}/${houseId}/order`,
+      addToWishList: houseId => `${housePrefix}/${houseId}/addToWishList`
+    }
+  },
+  user: {
+    get: {
+      userBasicInfo: userId => `${userPrefix}/${userId}`,
+      usernameExists: username =>
+        `${userPrefix}/usernameExists?username=${username}`,
+      emailExists: email => `${userPrefix}/emailExists?email=${email}`
+    },
+    post: {
+      wishList: () => `${userPrefix}/getWishList`,
+      addToWishList: () => `${userPrefix}/addToWishList`,
+      removeFromWishList: () => `${userPrefix}/removeFromWishList`,
+      signup: () => `${userPrefix}/signup`,
+      signin: () => `${userPrefix}/signin`,
+      isLoggedIn: () => `${userPrefix}/isLoggedIn`
+    }
+  }
+};
+
+export default urls;
