e03f4439f1fd4e329ee49abdbd08d2da1a38aa3e
Iamlooofeee <iamlooofeee@gmail.com>
2018-04-16 14:37:59 +0300

    search

--- server/services/search.js
+++ server/services/search.js
@@ -1,28 +1,40 @@
 import House from '../models/house';
+import User from '../models/user'
 
-export default async (req, res, next) => {
+export default (req, res, next) => {
 	let houses;
 	let wishList;
-	let session = req.session;
+	let user;
+	let session = req.body.session;
 	let search = req.query;
 	//
 	try {
-		await House.find({
+		House.find().where({
 			address: { $regex: search.q, $options: 'i' },
 			price: { $gt: search.minprice, $lt: search.maxprice },
 			rating: { $lt: search.maxrating, $gt: search.minrating }
-		}, function (err, results) {
+		}).limit(15).exec(function (err, results) {
 			houses = results.map(result => { return { isLiked: false, price: result.price, id: result._id, rating: result.rating, image: result.images[0] } })
-		}).limit(15);
-
-		await User.findOne({ _id: session }, function (err, user) {
-			houses = houses.map(house => { return { ...house, isLiked: user.wishList.indexOf(house.id) === -1 ? false : true } });
-		})
+			if (session) {
+				console.log(session)
+				user = User.findOne({ _id: session })
+					// console.log(user)
+					.exec(function (err, user) {
+						console.log(111111111111111111111111111111)
+						houses = houses.map(house => { return { ...houses, isLiked: user.wishList.indexOf(house.id) === -1 ? false : true } })
+						// console.log(houses);
+						res.send(houses)
+					})
+			} else {
+				res.send(houses);
+			}
+			res.send(houses)
+		});
 	} catch ({ message }) {
 		return next({
 			message: "Can't get all pages",
 			status: 302
 		});
 	}
-	res.send(houses);
+
 };
